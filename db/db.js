8793a460fbf0dbef4899e1d6baf9cdece8ce1ce1
felipe@kolekto.com.br-at-829167468932 <felipe@kolekto.com.br-at-829167468932>
2021-06-23 21:39:17 -0300

    heroku modifica

--- db/db.js
+++ db/db.js
@@ -1,6 +1,5 @@
 const {Client } = require('pg');
-const connectionString = 'postgres://pacbeedrkyykik:9ee0e9dadeac7ec719dec08658bc2bd39dd66f332bce2ad4b44c7de221876b03@ec2-52-4-111-46.compute-1.amazonaws.com:5432/d7dodtpnprjedd';
-
+const connectionString = 'postgres://jrvuxlyddeykth:bbb883e84cda13dcaeb964883ac329d34f0196f015aee09119aea60d3f5fb75d@ec2-52-23-45-36.compute-1.amazonaws.com:5432/da8jheg3r55do7';
 const client = new Client({
   connectionString: connectionString,
   ssl: {
